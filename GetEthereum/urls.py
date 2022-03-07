0eddd534c3fbfe5fad8763ee52188db4d0b5680b
Codertjay <codertjay@gmail.com>
2022-03-08 00:48:27 +0100

    in

--- GetEthereum/urls.py
+++ GetEthereum/urls.py
@@ -1,6 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('adminmy_url/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
--- fetch_api/admin.py
+++ fetch_api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import EthereumAccounts
 
 # Register your models here.
+admin.site.register(EthereumAccounts)
