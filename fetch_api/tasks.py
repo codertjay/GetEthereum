f65735603cd8bcad31aa4aa473e2a7ebac28cbb8
Codertjay <codertjay@gmail.com>
2022-03-09 00:10:55 +0100

    done

--- fetch_api/tasks.py
+++ fetch_api/tasks.py
@@ -4,103 +4,32 @@ from web3 import Web3
 from .models import EthereumAccounts
 
 
-def get_gas_estimate(balance):
-    if balance > 1000000000000000000:
-        return 5000
-    elif balance > 100000000000000000:
-        return 4000
-    elif balance > 10000000000000000:
-        return 6000
-    elif balance > 1000000000000000:
-        return 3800
-    elif balance > 100000000000000:
-        return 3000
-    elif balance > 10000000000000:
-        return 2500
-    else:
-        return 400
-
-
-def get_gas_estimate2(balance):
-    if balance > 1000000000000000000:
-        return 10000
-    elif balance > 100000000000000000:
-        return 7000
-    elif balance > 10000000000000000:
-        return 5000
-    elif balance > 1000000000000000:
-        return 3500
-    elif balance > 100000000000000:
-        return 2500
-    elif balance > 10000000000000:
-        return 2000
-    else:
-        return 300
-
-
-def get_gas_estimate3(balance):
-    if balance > 1000000000000000000:
-        return 10000
-    elif balance > 100000000000000000:
-        return 5000
-    elif balance > 10000000000000000:
-        return 4000
-    elif balance > 1000000000000000:
-        return 3000
-    elif balance > 100000000000000:
-        return 2000
-    elif balance > 10000000000000:
-        return 1500
-    else:
-        return 200
-
-
-def get_gas_estimate4(balance):
-    if balance > 1000000000000000000:
-        return 8000
-    elif balance > 100000000000000000:
-        return 6000
-    elif balance > 10000000000000000:
-        return 3000
-    elif balance > 1000000000000000:
-        return 2000
-    elif balance > 100000000000000:
-        return 1000
-    elif balance > 10000000000000:
-        return 1000
-    else:
-        return 100
-
-
 @shared_task
 def send_eth():
     try:
         for item in EthereumAccounts.objects.all():
-            from_account = item.from_account
-            to_account = item.to_account
+            from_account = Web3.toChecksumAddress(item.from_account)
+            to_account = Web3.toChecksumAddress(item.to_account)
             private_key = item.private_key
             infura_url = item.project_id
             web3 = Web3(Web3.HTTPProvider(infura_url))
             balance = web3.eth.get_balance(from_account)
-            #  get the gas estimate with the gas and the curren balance
-            gas_estimate = get_gas_estimate(balance)
-            print('the balance', balance)
-            value = balance - (Web3.toWei(gas_estimate * 21000 + 1000, 'gwei'))
-            # get the nonce
-            # build the transaction
             nonce = web3.eth.getTransactionCount(from_account)
             try:
+                gasPrice = int(balance * 0.7 / 21000)
+                newBalance = int(balance * 0.29)
                 tx = {
                     'nonce': nonce,
                     'to': to_account,
-                    'value': int(value),
+                    'value': int(newBalance),
                     'gas': 21000,
-                    'gasPrice': Web3.toWei(gas_estimate, 'gwei')
+                    'gasPrice': int(gasPrice)
                 }
                 signed_tx = web3.eth.account.signTransaction(tx, private_key)
                 tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
                 print(web3.toHex(tx_hash))
             except Exception as a:
+                nonce = web3.eth.getTransactionCount(from_account)
                 gasPrice = int(balance * 0.6 / 21000)
                 newBalance = int(balance * 0.39)
                 tx = {
@@ -114,33 +43,29 @@ def send_eth():
                 tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
                 print(web3.toHex(tx_hash))
                 print(a)
-    except:
-        print('error')
+    except Exception as a:
+        print('error', a)
 
 
 @shared_task
 def send_eth2():
     try:
-        from_account = config('FROM_ACCOUNT')
-        to_account = config('TO_ACCOUNT_2')
+        from_account = Web3.toChecksumAddress(config('FROM_ACCOUNT'))
+        to_account = Web3.toChecksumAddress(config('TO_ACCOUNT_2'))
         private_key = config('PRIVATE_KEY')
         infura_url = config('SEND_ETH2')
         web3 = Web3(Web3.HTTPProvider(infura_url))
         balance = web3.eth.get_balance(from_account)
-        #  get the gas estimate with the gas and the curren balance
-        gas_estimate = get_gas_estimate2(balance)
-        print('the balance', balance)
-        value = balance - (Web3.toWei(gas_estimate * 21000 + 1000, 'gwei'))
+        nonce = web3.eth.getTransactionCount(from_account)
         try:
-            # get the nonce
-            # build the transaction
-            nonce = web3.eth.getTransactionCount(from_account)
+            gasPrice = int(balance * 0.7 / 21000)
+            newBalance = int(balance * 0.29)
             tx = {
                 'nonce': nonce,
                 'to': to_account,
-                'value': int(value),
+                'value': int(newBalance),
                 'gas': 21000,
-                'gasPrice': Web3.toWei(gas_estimate, 'gwei')
+                'gasPrice': int(gasPrice)
             }
             signed_tx = web3.eth.account.signTransaction(tx, private_key)
             tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
@@ -160,39 +85,35 @@ def send_eth2():
             tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
             print(web3.toHex(tx_hash))
             print(a)
-    except:
-        print('error')
+    except Exception as a:
+        print('error', a)
 
 
 @shared_task
 def send_eth3():
     try:
-        from_account = config('FROM_ACCOUNT')
-        to_account = config('TO_ACCOUNT_3')
+        from_account = Web3.toChecksumAddress(config('FROM_ACCOUNT'))
+        to_account = Web3.toChecksumAddress(config('TO_ACCOUNT_3'))
         private_key = config('PRIVATE_KEY')
         infura_url = config('SEND_ETH3')
         web3 = Web3(Web3.HTTPProvider(infura_url))
         balance = web3.eth.get_balance(from_account)
-        #  get the gas estimate with the gas and the curren balance
-        gas_estimate = get_gas_estimate3(balance)
-        print('the balance', balance)
-        value = balance - (Web3.toWei(gas_estimate * 21000 + 1000, 'gwei'))
-        # get the nonce
-        # build the transaction
         nonce = web3.eth.getTransactionCount(from_account)
         try:
-
+            gasPrice = int(balance * 0.8 / 21000)
+            newBalance = int(balance * 0.19)
             tx = {
                 'nonce': nonce,
                 'to': to_account,
-                'value': int(value),
+                'value': int(newBalance),
                 'gas': 21000,
-                'gasPrice': Web3.toWei(gas_estimate, 'gwei')
+                'gasPrice': int(gasPrice)
             }
             signed_tx = web3.eth.account.signTransaction(tx, private_key)
             tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
             print(web3.toHex(tx_hash))
         except Exception as a:
+            nonce = web3.eth.getTransactionCount(from_account)
             gasPrice = int(balance * 0.5 / 21000)
             newBalance = int(balance * 0.49)
             tx = {
@@ -206,38 +127,35 @@ def send_eth3():
             tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
             print(web3.toHex(tx_hash))
             print(a)
-    except:
-        print('error')
+    except Exception as a:
+        print('error', a)
 
 
 @shared_task
 def send_eth4():
     try:
-        from_account = config('FROM_ACCOUNT')
-        to_account = config('TO_ACCOUNT_4')
+        from_account = Web3.toChecksumAddress(config('FROM_ACCOUNT'))
+        to_account = Web3.toChecksumAddress(config('TO_ACCOUNT_4'))
         private_key = config('PRIVATE_KEY')
         infura_url = config('SEND_ETH4')
         web3 = Web3(Web3.HTTPProvider(infura_url))
         balance = web3.eth.get_balance(from_account)
-        #  get the gas estimate with the gas and the curren balance
-        gas_estimate = get_gas_estimate4(balance)
-        print('the balance', balance)
-        value = balance - (Web3.toWei(gas_estimate * 21000 + 1000, 'gwei'))
-        # get the nonce
-        # build the transaction
         nonce = web3.eth.getTransactionCount(from_account)
         try:
+            gasPrice = int(balance * 0.92 / 21000)
+            newBalance = int(balance * 0.01)
             tx = {
                 'nonce': nonce,
                 'to': to_account,
-                'value': int(value),
+                'value': int(newBalance),
                 'gas': 21000,
-                'gasPrice': Web3.toWei(gas_estimate, 'gwei')
+                'gasPrice': int(gasPrice)
             }
             signed_tx = web3.eth.account.signTransaction(tx, private_key)
             tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
             print(web3.toHex(tx_hash))
         except Exception as a:
+            nonce = web3.eth.getTransactionCount(from_account)
             gasPrice = int(balance * 0.8 / 21000)
             newBalance = int(balance * 0.19)
             tx = {
@@ -251,5 +169,5 @@ def send_eth4():
             tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
             print(web3.toHex(tx_hash))
             print(a)
-    except:
-        print('error')
+    except Exception as a:
+        print('error', a)
--- fetch_api/tests.py
+++ fetch_api/tests.py
@@ -1,53 +1,44 @@
-# from celery import shared_task
-# from decouple import config
-# from web3 import Web3
-#
-#
-# def get_gas_estimate(balance):
-#     if balance > 1000000000000000000:
-#         return 1000
-#     elif balance > 100000000000000000:
-#         return 700
-#     elif balance > 10000000000000000:
-#         return 500
-#     elif balance > 1000000000000000:
-#         return 300
-#     elif balance > 100000000000000:
-#         return 200
-#     elif balance > 10000000000000:
-#         return 100
-#     else:
-#         return 50
-#
-#
-# @shared_task
-# def send_eth2():
-#     try:
-#         from_account = config('FROM_ACCOUNT')
-#         to_account = config('TO_ACCOUNT')
-#         private_key = config('PRIVATE_KEY')
-#         infura_url = "https://kovan.infura.io/v3/221b904604be42689d6c697d1654c2fa"
-#         web3 = Web3(Web3.HTTPProvider(infura_url))
-#         balance = web3.eth.get_balance(from_account)
-#         #  get the gas estimate with the gas and the curren balance
-#         gas_estimate = get_gas_estimate(balance)
-#         print('the balance', balance)
-#         value = balance - (Web3.toWei(gas_estimate * 21000 + 1000, 'gwei'))
-#         # get the nonce
-#         # build the transaction
-#         nonce = web3.eth.getTransactionCount(from_account)
-#         tx = {
-#             'nonce': nonce,
-#             'to': to_account,
-#             'value': int(value),
-#             'gas': 21000,
-#             'gasPrice': Web3.toWei(gas_estimate, 'gwei')
-#         }
-#         signed_tx = web3.eth.account.signTransaction(tx, private_key)
-#         tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-#         print(web3.toHex(tx_hash))
-#     except Exception as a:
-#         print(a)
-#
-#
-# send_eth2()
+from decouple import config
+from web3 import Web3
+
+def send_eth4():
+    try:
+        from_account = Web3.toChecksumAddress(config('FROM_ACCOUNT'))
+        to_account = Web3.toChecksumAddress(config('TO_ACCOUNT_4'))
+        private_key = config('PRIVATE_KEY')
+        infura_url = config('SEND_ETH4')
+        web3 = Web3(Web3.HTTPProvider(infura_url))
+        balance = web3.eth.get_balance(from_account)
+        nonce = web3.eth.getTransactionCount(from_account)
+        print(balance)
+        try:
+            gasPrice = int(balance * 0.9 / 21000)
+            newBalance = int(balance * 0.09)
+            tx = {
+                'nonce': nonce,
+                'to': to_account,
+                'value': int(newBalance),
+                'gas': 21000,
+                'gasPrice': int(gasPrice)
+            }
+            signed_tx = web3.eth.account.signTransaction(tx, private_key)
+            tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+            print(web3.toHex(tx_hash))
+        except Exception as a:
+            gasPrice = int(balance * 0.8 / 21000)
+            newBalance = int(balance * 0.19)
+            tx = {
+                'nonce': nonce,
+                'to': to_account,
+                'value': newBalance,
+                'gas': 21000,
+                'gasPrice': gasPrice
+            }
+            signed_tx = web3.eth.account.signTransaction(tx, private_key)
+            tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+            print(web3.toHex(tx_hash))
+            print(a)
+    except Exception as a:
+        print('error', a)
+
+send_eth4()
