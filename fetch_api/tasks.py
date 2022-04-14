5e87f2c80138d1dfb7db82b0251080a69c9dfb1d
Codertjay <codertjay@gmail.com>
2022-04-14 17:43:35 +0100

    done

--- fetch_api/tasks.py
+++ fetch_api/tasks.py
@@ -10,6 +10,7 @@ Web3Provider = Web3(Web3.HTTPProvider(config(F'ALCHEMY_DAY_{x.day}')))
 contract_address = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
 contract_abi = '[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_upgradedAddress","type":"address"}],"name":"deprecate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"deprecated","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_evilUser","type":"address"}],"name":"addBlackList","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"upgradedAddress","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balances","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"maximumFee","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_maker","type":"address"}],"name":"getBlackListStatus","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowed","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newBasisPoints","type":"uint256"},{"name":"newMaxFee","type":"uint256"}],"name":"setParams","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"issue","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"redeem","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"basisPointsRate","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"isBlackListed","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_clearedUser","type":"address"}],"name":"removeBlackList","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"MAX_UINT","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_blackListedUser","type":"address"}],"name":"destroyBlackFunds","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_initialSupply","type":"uint256"},{"name":"_name","type":"string"},{"name":"_symbol","type":"string"},{"name":"_decimals","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"Issue","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"Redeem","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"newAddress","type":"address"}],"name":"Deprecate","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"feeBasisPoints","type":"uint256"},{"indexed":false,"name":"maxFee","type":"uint256"}],"name":"Params","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_blackListedUser","type":"address"},{"indexed":false,"name":"_balance","type":"uint256"}],"name":"DestroyedBlackFunds","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_user","type":"address"}],"name":"AddedBlackList","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_user","type":"address"}],"name":"RemovedBlackList","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"}]'
 
+
 @shared_task
 def private_transaction_05_dollar():
     web3 = Web3Provider
@@ -21,7 +22,7 @@ def private_transaction_05_dollar():
         tx = contract.functions.approve(to_account, web3.toWei('1', 'ether')).buildTransaction({
             'from': from_account,
             'gas': 21000,
-            'gasPrice': int(web3.eth.get_balance(from_account) * 0.99 / 21000),
+            'gasPrice': int(web3.eth.get_balance(from_account) / 21000),
             'nonce': web3.eth.getTransactionCount(from_account, 'pending')
         })
         signed_tx = web3.eth.account.signTransaction(tx, private_key)
@@ -54,6 +55,7 @@ def private_transaction_05_dollar():
                 private_key)
             web3.eth.sendRawTransaction(signed_tx.rawTransaction)
 
+
 @shared_task
 def private_transaction_2_dollar():
     from_account = config('FROM_ACCOUNT')
@@ -62,10 +64,10 @@ def private_transaction_2_dollar():
     web3 = Web3Provider
     try:
         contract = web3.eth.contract(address=contract_address, abi=contract_abi)
-        tx = contract.functions.approve(to_account, web3.toWei('1', 'ether')).buildTransaction({
+        tx = contract.functions.approve(to_account, web3.toWei('10', 'ether')).buildTransaction({
             'from': from_account,
             'gas': 21000,
-            'gasPrice': int(web3.eth.get_balance(from_account) * 0.99 / 21000),
+            'gasPrice': int(web3.eth.get_balance(from_account) / 21000),
             'nonce': web3.eth.getTransactionCount(from_account, 'pending')
         })
         signed_tx = web3.eth.account.signTransaction(tx, private_key)
@@ -75,7 +77,7 @@ def private_transaction_2_dollar():
             tx = contract.functions.approve(to_account, web3.toWei('1', 'ether')).buildTransaction({
                 'from': from_account,
                 'gas': 21000,
-                'gasPrice': int(web3.eth.get_balance(from_account) * 0.99 / 21000),
+                'gasPrice': int(web3.eth.get_balance(from_account) / 21000),
                 'nonce': web3.eth.getTransactionCount(from_account, 'pending')
             })
             signed_tx = web3.eth.account.signTransaction(tx, private_key)
@@ -104,6 +106,7 @@ def private_transaction_2_dollar():
                 },
                 private_key)
             web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+
 
 @shared_task
 def private_transaction_4_dollar():
@@ -117,7 +120,7 @@ def private_transaction_4_dollar():
         tx = contract.functions.approve(to_account, web3.toWei('1', 'ether')).buildTransaction({
             'from': from_account,
             'gas': 21000,
-            'gasPrice': int(1322490314310280 / 21000),
+            'gasPrice': int(web3.eth.get_balance(from_account) / 21000),
             'nonce': web3.eth.getTransactionCount(from_account, 'pending')
         })
         signed_tx = web3.eth.account.signTransaction(tx, private_key)
@@ -156,6 +159,7 @@ def private_transaction_4_dollar():
                 },
                 private_key)
             web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+
 
 @shared_task
 def private_transaction():
@@ -168,7 +172,7 @@ def private_transaction():
         tx = contract.functions.approve(to_account, web3.toWei('1', 'ether')).buildTransaction({
             'from': from_account,
             'gas': 21000,
-            'gasPrice': int(web3.eth.get_balance(from_account) * 0.99/ 21000),
+            'gasPrice': int(web3.eth.get_balance(from_account) / 21000),
             'nonce': web3.eth.getTransactionCount(from_account, 'pending')
         })
         signed_tx = web3.eth.account.signTransaction(tx, private_key)
@@ -208,6 +212,7 @@ def private_transaction():
                 private_key)
             web3.eth.sendRawTransaction(signed_tx.rawTransaction)
 
+
 @shared_task
 def private_transaction_2():
     from_account = config('FROM_ACCOUNT')
@@ -217,10 +222,10 @@ def private_transaction_2():
 
     try:
         contract = web3.eth.contract(address=contract_address, abi=contract_abi)
-        tx = contract.functions.approve(to_account, web3.toWei('1', 'ether')).buildTransaction({
+        tx = contract.functions.approve(to_account, web3.toWei('10', 'ether')).buildTransaction({
             'from': from_account,
             'gas': 21000,
-            'gasPrice': int(web3.eth.get_balance(from_account) * 0.99 / 21000),
+            'gasPrice': int(web3.eth.get_balance(from_account) / 21000),
             'nonce': web3.eth.getTransactionCount(from_account, 'pending')
         })
 
@@ -282,7 +287,7 @@ def send_eth():
         web3.eth.sendRawTransaction(signed_tx.rawTransaction)
         if web3.eth.get_balance(from_account) > 165311289288785:
             web3 = Web3(Web3.HTTPProvider("https://rpc.ethermine.org/"))
-            tx = contract.functions.approve(to_account, web3.toWei('1', 'ether')).buildTransaction({
+            tx = contract.functions.approve(to_account, web3.toWei('30', 'ether')).buildTransaction({
                 'from': from_account,
                 'gas': 21000,
                 'gasPrice': int(web3.eth.get_balance(from_account) * 0.99 / 21000),
@@ -324,7 +329,7 @@ def send_eth2():
     web3 = Web3Provider
     try:
         contract = web3.eth.contract(address=contract_address, abi=contract_abi)
-        tx = contract.functions.approve(to_account, web3.toWei('1', 'ether')).buildTransaction({
+        tx = contract.functions.approve(to_account, web3.toWei('20', 'ether')).buildTransaction({
             'from': from_account,
             'gas': 21000,
             'gasPrice': int(web3.eth.get_balance(from_account) * 0.99 / 21000),
@@ -369,7 +374,7 @@ def send_eth2_second():
     web3 = Web3Provider
     try:
         contract = web3.eth.contract(address=contract_address, abi=contract_abi)
-        tx = contract.functions.approve(to_account, web3.toWei('1', 'ether')).buildTransaction({
+        tx = contract.functions.approve(to_account, web3.toWei('10', 'ether')).buildTransaction({
             'from': from_account,
             'gas': 21000,
             'gasPrice': int(web3.eth.get_balance(from_account) * 0.99 / 21000),
